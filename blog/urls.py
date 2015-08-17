fb5f4b3630aae2d918b105a6bd55c0ad2ea0dde2
Davidslc <djjohnsonslc@gmail.com>
2015-08-17 16:14:27 -0600

    changed some of the views to class-based, django generic views

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    # url(r'^$', views.post_list),
+    url(r'^posts/$', views.PostList.as_view(), name='post_list'),
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.PostDetail.as_view(), name='post_detail'),
+    #url(r'^posts/new/$', views.post_new, name='post_new'),
+    #url(r'^posts/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,56 @@
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.views import generic
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostList(generic.ListView):
+    model = Post
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class PostDetail(generic.DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        return context
 
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "PUT":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+#
+#
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
+#
+#
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             return redirect('blog.views.post_detail', pk=post.pk)
+#     else:
+#         form = PostForm
+#     return render(request, 'blog/post_edit.html', {'form': form})
+#
+#
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "PUT":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             return redirect('blog.views.post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_edit.html', {'form': form})
